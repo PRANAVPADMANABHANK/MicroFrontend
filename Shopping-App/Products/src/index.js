e77d139fa9f5f63f24e761af1c3213d7f04fd8db
pranav <pranav.k@mitz.ai>
2024-03-03 23:14:42 +0530

    feat: container added and mfe implemented

--- Shopping-App/Products/src/index.js
+++ Shopping-App/Products/src/index.js
@@ -4,8 +4,8 @@ import faker from 'faker'
 let productString = ""
 
 
-for( let i = 0; i < 5; i++){
-    productString += `<div>${faker.commerce.productName()}</div>`
+for( let i = 0; i < 10; i++){
+    productString += `<div>${i+1}. ${faker.commerce.productName()}</div>`
 }
 
 
--- Shopping-App/Products/webpack.config.js
+++ Shopping-App/Products/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 
 module.exports = {
     mode: "development",
@@ -8,6 +9,13 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './public/index.html'
+        }), 
+        new ModuleFederationPlugin({
+            name: 'products',                      //name of the MFE
+            filename: 'remoteEntry.js',            //name of a file which will be imported in container 
+            exposes: {                             //code to be exported and can be used in container
+                './ProductIndex': './src/index'
+            }
         })
     ]
-}
+}        
