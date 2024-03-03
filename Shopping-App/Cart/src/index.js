1f6419c5d749d1d18ed86c465c8ad07a9da125e2
pranav <pranav.k@mitz.ai>
2024-03-03 13:08:30 +0530

    feat: cart MFE added

---
+++ Shopping-App/Cart/src/index.js
@@ -0,0 +1,6 @@
+import faker from 'faker'
+
+
+const cartString = `<div>You have ${faker.datatype.number(20)} items in your cart!</div>`
+
+document.querySelector('#cart-list').innerHTML = cartString
